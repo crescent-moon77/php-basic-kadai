17c562b4d8935a78a59068b9acaffe9c3b041aeb
crescent-moon77 <kazuki.n.j25@gmail.com>
2025-08-26 00:50:52 +0900

    課題をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>第16章課題</title>
+</head>
+
+<body>
+   <p>
+    <?php
+    class Food{
+      private $name;
+      private $price;
+
+      public function __construct(string $name, int $price){
+        $this->name=$name;
+        $this->price=$price;
+      }
+
+      public function show_price(){
+        echo $this->price;
+      }
+    }
+    class Animal{
+      private $name;
+      private $height;
+      private $weight;
+
+      public function __construct(string $name, int $height, int $weight){
+        $this->name=$name;
+        $this->height=$height;
+        $this->weight=$weight;
+      }
+      public function show_height(){
+        echo $this->height;
+      }
+    }
+    $food= new Food( "tomato",300);
+    print_r($food);
+    echo "<br/>";
+    
+    $animal= new Animal("cat",70,3000);
+    print_r($animal);
+    echo "<br/>";
+
+    $food->show_price();
+    echo "<br/>";
+    $animal->show_height();
+    ?>
+       
+   </p>
+</body>
+
+</html>
